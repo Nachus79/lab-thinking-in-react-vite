417edaa775b561c8fd137554caeb00e8ca0d5199
Nacho Monzón <ignaciomonzona@gmail.com>
2024-12-02 18:03:27 +0100

    Corrections

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,7 +1,11 @@
 function ProductRow() {
-
-
-
+  const { name, price, inStock } = product;
+  return (
+    <tr style={{ color: inStock ? "black" : "red" }}>
+      <td>{name}</td>
+      <td>{price}</td>
+    </tr>
+  );
 }
 
 export default ProductRow;
